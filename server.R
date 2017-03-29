077b45309b399ec377cd7d1da981bf5dc5f31a89
Ashish Chaturvedi <me.ashish005@gmail.com>
2017-03-29 14:26:06 +0530

    init setup

---
+++ server.R
@@ -0,0 +1,23 @@
+
+# This is the server logic for a Shiny web application.
+# You can find out more about building applications with Shiny here:
+#
+# http://shiny.rstudio.com
+#
+
+library(shiny)
+library(datasets)
+
+shinyServer(function(input, output, session) {
+
+  output$messageMenu <- renderMenu({
+    # Code to generate each of the messageItems here, in a list. This assumes
+    # that messageData is a data frame with two columns, 'from' and 'message'.
+    msgs <- apply(messageData, 1, function(row) {
+      messageItem(from = row[["from"]], message = row[["message"]])
+    })
+    # This is equivalent to calling:
+    #   dropdownMenu(type="messages", msgs[[1]], msgs[[2]], ...)
+    dropdownMenu(type = "messages", .list = msgs)
+  })
+})
